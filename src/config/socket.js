bcafe78c53c754fc8a6c33bf802f0e719001c795
truongvd05 <truongvdf8@fullstack.edu.vn>
2026-03-10 06:57:43 +0700

    update cors socket

--- src/config/socket.js
+++ src/config/socket.js
@@ -5,9 +5,11 @@ export function initSocket(server) {
     const io = new Server(server, {
         cors: {
             origin: [
-                "http://localhost:5173",
-                "http://127.0.0.1:5173",
                 "https://truongvd05.github.io",
+                "http://103.118.29.46",
+                "http://chatdemo.site",
+                "https://chatdemo.site",
+                process.env.CLIENT_URL,
             ],
             methods: ["GET", "POST"],
             credentials: true,
